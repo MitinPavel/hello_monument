ef0656c030c45750884683e480e640e4f07b2160
pm <mitin.pavel@gmail.com>
2014-09-03 12:32:03 +0300

    Add a spec on a constrain: tags of different users should be independent.

--- spec/controllers/monuments_controller_spec.rb
+++ spec/controllers/monuments_controller_spec.rb
@@ -27,6 +27,26 @@ RSpec.describe MonumentsController, :type => :controller do
     it_behaves_like "a controller which requires authentication" do
       let(:action) { put :update, default_params }
     end
+
+    context "[monument categories]" do
+      let(:john) { create :user }
+      let(:jill) { create :user }
+
+      before { sign_in john }
+
+      it "treats tags of different users as completely independent" do
+        johns_monument = create :monument, collection: create(:collection, user: john), category_list: %w(poet)
+        jills_monument = create :monument, collection: create(:collection, user: jill), category_list: %w(poet)
+
+        put :update,
+            collection_id: johns_monument.collection.id,
+            id: johns_monument.id,
+            monument: {category_list: 'songster'}
+
+        expect(johns_monument.reload.category_list).to eq(["songster"])
+        expect(jills_monument.reload.category_list).to eq(["poet"])
+      end
+    end
   end
 
   describe "#destroy" do
