44e99b489f551acef631569ce1f1656cc7ce1895
pm <mitin.pavel@gmail.com>
2014-09-03 09:54:21 +0300

    Add specs for the collection controller.

--- spec/controllers/collections_controller_spec.rb
+++ spec/controllers/collections_controller_spec.rb
@@ -1,15 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe CollectionsController, :type => :controller do
+  describe "#index" do
+    it_behaves_like "a controller which requires authentication" do
+      let(:action) { get :index }
+    end
+  end
+
   describe "#new" do
     it_behaves_like "a controller which requires authentication" do
       let(:action) { get :new }
     end
   end
 
-  describe "#index" do
+  describe "#create" do
     it_behaves_like "a controller which requires authentication" do
-      let(:action) { get :index }
+      let(:action) { post :create }
+    end
+  end
+
+  describe "#edit" do
+    it_behaves_like "a controller which requires authentication" do
+      let(:action) { get :edit, id: create(:collection).id }
+    end
+  end
+
+  describe "#update" do
+    it_behaves_like "a controller which requires authentication" do
+      let(:action) { put :update, id: create(:collection).id }
+    end
+  end
+
+  describe "#destroy" do
+    it_behaves_like "a controller which requires authentication" do
+      let(:action) { delete :destroy, id: create(:collection).id }
     end
   end
 end
