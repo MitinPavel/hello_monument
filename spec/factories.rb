2907b88f086af9bd7a1b744358283e3e46d9f780
pm <mitin.pavel@gmail.com>
2014-09-03 09:53:31 +0300

    Fix spec factories: generate a uniq email for eac user.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    email Faker::Internet.email
-    password Faker::Internet.password
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
   end
 
   factory :collection do
-    name Faker::Lorem.sentence
+    name { Faker::Lorem.sentence }
     user
   end
 end
