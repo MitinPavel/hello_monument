0ffbb5f3bdb22a2b06afa38ef25a6fbd183bbc05
pm <mitin.pavel@gmail.com>
2014-09-03 06:52:50 +0300

    Introduce a cuke for collections: create a collection.

---
+++ spec/features/collections_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "management of collections", :type => :feature do
+  include_context "authenticated user"
+
+  specify "a user can create a collection" do
+    ::Dom::MainMenu.instance.visit_new_collection
+
+    expect {
+      ::Dom::CollectionForm.instance.submit_empty_form
+    }.to change { ::Dom::CollectionForm.instance.errors }.from([]).to(["name should not be blank"])
+
+    expect {
+      ::Dom::CollectionForm.instance.submit name: "Ljubljana 2014"
+    }.to change { page.has_content? 'Was created.' }.from false
+
+    ::Dom::MainMenu.instance.visit_collections
+
+    expect(page).to have_content("Ljubljana 2014")
+  end
+end
